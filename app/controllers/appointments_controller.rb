c7a7af49e20a5cd3fbfad731465bdb5d5a6869bf
Polly Sileo <pollysileo@gmail.com>
2015-07-25 21:30:58 -0400

    Update Appoinment with testing coverage

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -4,18 +4,19 @@ class AppointmentsController < ApplicationController
   end
 
   def create
-    appointment = Appointment.create!(appointments_params)
+    appointment = Appointment.create!(appointment_params)
     render json: appointment
   end
 
   def update
-    @appointment = Appointment.find(params[:id])
-    @appointment.update!(showed: params[:showed], pantry_day_id: params[:pantry_day_id], client_id: params[:client_id])
+    appointment = Appointment.find(params[:id])
+    appointment.update!(appointment_params)
+    render json: appointment
   end
 
 private
 
-  def appointments_params
-    params.require(:appointments).permit(:pantry_day_id, :client_id, :showed)
+  def appointment_params
+    params.require(:appointment).permit(:id, :pantry_day_id, :client_id, :showed)
   end
 end
--- test/controllers/appointments_controller_test.rb
+++ test/controllers/appointments_controller_test.rb
@@ -5,7 +5,8 @@ class AppointmentsControllerTest < ActionController::TestCase
   def test_appointments_can_be_created
     # PantryDay.create!(id: 1, date_time: DateTime.new(2015, 2, 3, 11), num_volunteers: 4, max_num_clients: 3)
     # post :create, item: { name: "Thing", price: 10 }
-    post :create, appointments: { pantry_day_id: 5, client_id: 6 }
+    assert_equal 0, Appointment.count
+    post :create, appointment: { pantry_day_id: 5, client_id: 6 }
     assert_equal "200", response.code
     # response = JSON.parse(response.body)
     assert_equal 5, JSON.parse(response.body)["pantry_day_id"]
@@ -13,6 +14,7 @@ class AppointmentsControllerTest < ActionController::TestCase
   end
 
   def test_appointment_can_be_updated
-    
+    Appointment.create!(id: 111, pantry_day_id: 3, client_id: 4)
+    patch :update, id: 111, appointment: { showed: "true"}
   end
 end
