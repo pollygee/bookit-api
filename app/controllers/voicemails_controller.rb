43bb4059fd3eb796a599dfb302edcb2c297e16df
Polly Sileo <pollysileo@gmail.com>
2015-07-29 14:52:22 -0400

    Fix syntax error

--- app/controllers/voicemails_controller.rb
+++ app/controllers/voicemails_controller.rb
@@ -17,6 +17,6 @@ private
 
   def voicemail_params
     body_data = JSON.parse(request.body.read)
-    ActionController::Parameters.new(body_data)require(:voicemail).permit(:id, :client_id, :resolved, :out_of_area, :above_income, :needs_other, :no_docs, :time_with_call)
+    ActionController::Parameters.new(body_data).require(:voicemail).permit(:id, :client_id, :resolved, :out_of_area, :above_income, :needs_other, :no_docs, :time_with_call)
   end
 end
