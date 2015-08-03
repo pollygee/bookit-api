2d333e27f9cea370c76ef4e994df477afeb4c540
Polly Sileo <pollysileo@gmail.com>
2015-08-03 11:20:00 -0400

    Sort clients names for database display

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,6 +1,6 @@
 class ClientsController < ApplicationController
   def index
-    @clients = Client.all.includes(:pantry_days)
+    @clients = Client.all.includes(:pantry_days).order(:last_name)
   end
 
   def show
